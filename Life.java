82614b10264af8002d2cd09da0351ee6e5f7a677
IgorDurovic <id.tesla@gmail.com>
2014-12-31 20:27:25 -0700

    grid revision

--- Life.java
+++ Life.java
@@ -16,29 +16,24 @@ public class Life extends PApplet {
 
 int millis = 100;
 int cellDim = 5;
+int mortalityRate;
+int population;
 
-int cells[][];
-int savedCells[][];
+Cell grid[][];
+Cell savedGrid[][];
 int lastTime = 0;
 
 public void setup(){
   size(1280, 720);
   
-  cells = new int[width/cellDim][height/cellDim];
-  savedCells = new int[width/cellDim][height/cellDim];
+  grid = new Cell[width/cellDim][height/cellDim];
+  savedGrid = new Cell[width/cellDim][height/cellDim];
 
   stroke(48);
 
   for(int a = 0; a < width/cellDim; a++){
     for(int b = 0; b < height/cellDim; b++){
-      int state = PApplet.parseInt(random(100));
-      if(state > 15){
-        state = 0;
-      }
-      else{
-        state = 1;
-      }
-      cells[a][b] = state;
+      grid[a][b] = new Cell(50, 10, 10);
     }
   }
   background(256, 256, 256);
@@ -47,12 +42,7 @@ public void setup(){
 public void draw(){
   for(int a = 0; a < width/cellDim; a++){
     for(int b = 0; b < height/cellDim; b++){
-      if(cells[a][b] == 1){
-        fill(0);
-      }
-      else{
-        fill(256,150,150);
-      }
+      fill(256, 256*grid[a][b].getHealthy(), 256*grid[a][b].getHealthy());
       rect(cellDim*a, cellDim*b, cellDim, cellDim);
     }
   }
@@ -68,38 +58,41 @@ public void keyPressed(){
 }
 
 public void cycle(){
-  for (int a=0; a<width/cellDim; a++) {
-    for (int b=0; b<height/cellDim; b++) {
-      savedCells[a][b] = cells[a][b];
-    }
-  }
-  
-  for(int a = 0; a < width/cellDim; a++){
-    for(int b = 0; b < height/cellDim; b++){
-      int neighbors = 0;
-      for(int c = a-1; c <= a+1; c++){
-        for(int d = b-1; d <= b+1; d++){
-          if(((c>=0)&&(c<width/cellDim))&&((d>=0)&&(d<height/cellDim))) { // Make sure you are not out of bounds
-            if(!((c==a)&&(d==b))) { // Make sure to to check against self
-              if(savedCells[c][d]==1){
-                neighbors++; // Check alive neighbours and count them
-              }
-            } // End of if
-          }
-        }
-      }
-      if(savedCells[a][b] == 1){
-        if(neighbors<2||neighbors>3){
-          cells[a][b] = 0;
-        }
-      }
-      else{
-        if(neighbors == 3){
-          cells[a][b] = 1;
-        }
-      }
-    }
-  }
+	
+//OUTDATED CODE, TO BE UPDATED
+	
+//  for (int a=0; a<width/cellDim; a++) {
+//    for (int b=0; b<height/cellDim; b++) {
+//      savedCells[a][b] = cells[a][b];
+//    }
+//  }
+//  
+//  for(int a = 0; a < width/cellDim; a++){
+//    for(int b = 0; b < height/cellDim; b++){
+//      int neighbors = 0;
+//      for(int c = a-1; c <= a+1; c++){
+//        for(int d = b-1; d <= b+1; d++){
+//          if(((c>=0)&&(c<width/cellDim))&&((d>=0)&&(d<height/cellDim))) { // Make sure you are not out of bounds
+//            if(!((c==a)&&(d==b))) { // Make sure to to check against self
+//              if(savedCells[c][d]==1){
+//                neighbors++; // Check alive neighbours and count them
+//              }
+//            } // End of if
+//          }
+//        }
+//      }
+//      if(savedCells[a][b] == 1){
+//        if(neighbors<2||neighbors>3){
+//          cells[a][b] = 0;
+//        }
+//      }
+//      else{
+//        if(neighbors == 3){
+//          cells[a][b] = 1;
+//        }
+//      }
+//    }
+//  }
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "--full-screen", "--bgcolor=#666666", "--stop-color=#cccccc", "Life" };
@@ -110,3 +103,4 @@ public void cycle(){
     }
   }
 }
+
