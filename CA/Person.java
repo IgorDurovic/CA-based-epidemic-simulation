a01ecc89204114581883d30de80cbb5c3b42145d
IgorDurovic <id.tesla@gmail.com>
2015-01-26 20:32:23 -0700

    person attributes added
    getters and setters along with their designated attributes have been added. Code will be refactored soon.

--- CA/Person.java
+++ CA/Person.java
@@ -1,4 +1,30 @@
-
 public class Person {
+	
+	private status personStatus;
+	private double infectionRate;
+	
+	public enum status{
+		SUSCEPTIBLE, INFECTED, REMOVED
+	}
+	
+	public double getInfectionRate() {
+		return infectionRate;
+	}
 
+	public void setInfectionRate(double infectionRate) {
+		this.infectionRate = infectionRate;
+	}
+
+	Person(double ir) {
+		personStatus = status.SUSCEPTIBLE;
+		infectionRate = ir;
+	}
+
+	public status getPersonStatus() {
+		return personStatus;
+	}
+
+	public void setPersonStatus(status personStatus) {
+		this.personStatus = personStatus;
+	}
 }
