fff0b653aa8def6ddb404dfc1f77eccd20e80911
IgorDurovic <id.tesla@gmail.com>
2015-02-08 09:52:19 -0700

    reformatting

--- CA/Person.java
+++ CA/Person.java
@@ -1,40 +1,49 @@
-public class Person {
+public class Person 
+{
 	
 	private status personStatus;
 	private double infectionRate;
 	public int recoveryTime;
 
-	public enum status{
+	public enum status
+	{
 		SUSCEPTIBLE, INFECTED, REMOVED
 	}
 
-	Person(double ir) {
+	Person(double ir) 
+	{
 		personStatus = status.SUSCEPTIBLE;
 		infectionRate = ir;
 	}
 
-	public double getInfectionRate() {
+	public double getInfectionRate() 
+	{
 		return infectionRate;
 	}
 
-	public void setInfectionRate(double infectionRate) {
+	public void setInfectionRate(double infectionRate) 
+	{
 		this.infectionRate = infectionRate;
 	}
 
-	public status getPersonStatus() {
+	public status getPersonStatus() 
+	{
 		return personStatus;
 	}
 
-	public void setPersonStatus(status personStatus, int rt) {
+	public void setPersonStatus(status personStatus, int rt) 
+	{
 		this.personStatus = personStatus;
 		recoveryTime = rt;
 	}
 
-	public void getRecoveryTime(){
+	public void getRecoveryTime()
+	{
 		return recoveryTime;
 	}
 
-	public void setRecoveryTime(int recoveryTime){
+	public void setRecoveryTime(int recoveryTime)
+	{
 		this.recoveryTime = recoveryTime;
 	}
 }
