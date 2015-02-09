3ae61f899715133c398fad046000933eca66e823
IgorDurovic <id.tesla@gmail.com>
2015-02-08 23:39:13 -0700

    debugging

--- CA/Life.java
+++ CA/Life.java
@@ -1,30 +1,21 @@
 import processing.core.*;
-import processing.data.*;
-import processing.event.*;
-import processing.opengl.*;
 
-import java.util.HashMap;
 import java.util.ArrayList;
-import java.io.File;
-import java.io.BufferedReader;
-import java.io.PrintWriter;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.IOException;
+import java.util.Random;
 
 public class Life extends PApplet 
 {
-
-	int millis = 100;
+	int millis = 500;
 	int cellDim = 25;
-	int mortalityRate 0.05;
+	static double mortalityRate = 0.01;
 	int population;
 
 	Cell grid[][];
 	Cell savedGrid[][];
 	int lastTime = 0;
-
-	public void setup()  //how big the enviroment is going to be
+	int count = 0;
+	
+	public void setup() 
 	{
 		size(1280, 720);
 
@@ -37,21 +28,20 @@ public class Life extends PApplet
 		{
 			for (int b = 0; b < height / cellDim; b++) 
 			{
-				grid[a][b] = new Cell(50, 10, 10, 0.1);
+				grid[a][b] = new Cell(50, 10, 10, 0.05);
 			}
 		}
 		background(256, 256, 256);
 	}
 
-	public void draw() // how big each person's area is going to be
+	public void draw() 
 	{
 		for (int a = 0; a < width / cellDim; a++) 
 		{
 			for (int b = 0; b < height / cellDim; b++) 
 			{
 				///////////
-				fill(256, 256 * grid[a][b].getHealthy(),
-						256 * grid[a][b].getHealthy());
+				fill(256, Math.round(256.0*grid[a][b].getHealthy()), Math.round(256.0*grid[a][b].getHealthy()));
 				rect(cellDim * a, cellDim * b, cellDim, cellDim);
 				///////////
 			}
@@ -81,37 +71,21 @@ public class Life extends PApplet
 				while(!immigrants.isEmpty())
 				{
 					int x = a, y = b;
-					int directionX;
-					if(Math.random() > 0.5)
-					{
-						directionX = 1;
-					}
-					else
-					{
-						directionX = -1;
-					}
+					Random rndobj = new Random();
+					int xRand = rndobj.nextInt(3) - 1;
+					int yRand = rndobj.nextInt(3) - 1;
 					
-					int directionY;
-					if(Math.random() > 0.5)
+					if((x + xRand < 0) || (grid.length <= x + xRand))
 					{
-						directionY = 1;
-					}
-					else
-					{
-						directionY = -1;
-					}
-					
-					if((x + directionX < 0) || (grid.length < x + directionX))
-					{
-						directionX = -directionX;
+						xRand = -xRand;
 					}
 
-					if((y + directionY < 0) || (grid.length < x + directionY))
+					if((y + yRand < 0) || (grid[0].length <= y + yRand))
 					{
-						directionY = -directionY;
+						yRand = -yRand;
 					}
-					x += directionX;
-					y += directionY;
+					x += xRand;
+					y += yRand;
 					
 					if(grid[x][y].getCellPopulation() < grid[x][y].getCarryingCapacity())
 					{
@@ -138,39 +112,11 @@ public class Life extends PApplet
 		{
 			for(int b = 0; b < height/cellDim; b++)
 			{
-				grid.[a][b].recovery();
-				grid.[a][b].death();
-				grid.[a][b].graphing();
+				grid[a][b].recovery();
+				grid[a][b].death();
+				grid[a][b].graphing();
 			}
 		}
-
-		// for(int a = 0; a < width/cellDim; a++){
-		// for(int b = 0; b < height/cellDim; b++){
-		// int neighbors = 0;
-		// for(int c = a-1; c <= a+1; c++){
-		// for(int d = b-1; d <= b+1; d++){
-		// if(((c>=0)&&(c<width/cellDim))&&((d>=0)&&(d<height/cellDim))) { //
-		// Make sure you are not out of bounds
-		// if(!((c==a)&&(d==b))) { // Make sure to to check against self
-		// if(savedCells[c][d]==1){
-		// neighbors++; // Check alive neighbours and count them
-		// }
-		// } // End of if
-		// }
-		// }
-		// }
-		// if(savedCells[a][b] == 1){
-		// if(neighbors<2||neighbors>3){
-		// cells[a][b] = 0;
-		// }
-		// }
-		// else{
-		// if(neighbors == 3){
-		// cells[a][b] = 1;
-		// }
-		// }
-		// }
-		// }
 	}
 
 	static public void main(String[] passedArgs) 
