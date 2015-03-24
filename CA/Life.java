aa6623871865362d22ba056d08620c4027e537b7
IgorDurovic <id.tesla@gmail.com>
2015-03-23 22:48:34 -0600

    graphing support

--- CA/Life.java
+++ CA/Life.java
@@ -3,18 +3,47 @@ import processing.core.*;
 import java.util.ArrayList;
 import java.util.Random;
 
+import java.awt.Color;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.RefineryUtilities;
+
 public class Life extends PApplet 
 {
-	int millis = 500;
+	int millis = 100;
 	int cellDim = 25;
 	public static boolean bool = false;
-	static double mortalityRate = 0.01;
+	static double mortalityRate = 0.34620418848;
 	int population;
+	
+	int susceptiblePopulation;
+	int infectedPopulation;
+	int recoveredPopulation;
+	int deaths;
 
 	Cell grid[][];
 	Cell savedGrid[][];
 	int lastTime = 0;
 	int count = 0;
+
+    final static XYSeries series1 = new XYSeries("Susceptible");
+    final static XYSeries series2 = new XYSeries("Infected");
+    final static XYSeries series3 = new XYSeries("Recovered");
+    final static XYSeries series4 = new XYSeries("Deaths");
+    final static XYSeriesCollection dataset = new XYSeriesCollection();
+    
+    final static XYDataset dataset1 = createDataset();
+    final static JFreeChart chart = createChart(dataset1);
+    final static ChartPanel chartPanel = new ChartPanel(chart);
 	
 	public void setup() 
 	{
@@ -31,10 +60,10 @@ public class Life extends PApplet
 			{
 				if(a == (width/cellDim)/2 && b == (height/cellDim)/2){
 					bool = true;
-					grid[a][b] = new Cell(50, 10, 10, 0.05);
+					grid[a][b] = new Cell(50, 10, 10, 0.2);
 				}
 				else{
-					grid[a][b] = new Cell(50, 10, 10, 0.05);
+					grid[a][b] = new Cell(50, 10, 10, 0.2);
 				}
 			}
 		}
@@ -48,7 +77,8 @@ public class Life extends PApplet
 			for (int b = 0; b < height / cellDim; b++) 
 			{
 				///////////
-				fill(256, Math.round(256.0*grid[a][b].getHealthy()), Math.round(256.0*grid[a][b].getHealthy()));
+				fill(256, Math.round(256.0*grid[a][b].getHealthy()), 
+						Math.round(256.0*grid[a][b].getHealthy()));
 				rect(cellDim * a, cellDim * b, cellDim, cellDim);
 				///////////
 			}
@@ -56,6 +86,7 @@ public class Life extends PApplet
 
 		if (millis() - lastTime > millis) 
 		{
+			
 			cycle();
 			lastTime = millis();
 		}
@@ -68,8 +99,12 @@ public class Life extends PApplet
 
 	public void cycle() 
 	{
+		infectedPopulation = 0;
+		susceptiblePopulation = 0;
+		recoveredPopulation = 0;
+		
 		//movement phase
-		ArrayList<Person> immigrants;
+		ArrayList<Person> immigrants = null;
 		for (int a = 0; a < width / cellDim; a++) 
 		{
 			for (int b = 0; b < height / cellDim; b++) 
@@ -79,6 +114,34 @@ public class Life extends PApplet
 				{
 					int x = a, y = b;
 					Random rndobj = new Random();
+					/*if(quot < 0){
+						prob = 0-rndobj.nextDouble();
+						if(prob > quot){
+							yRand = -1;
+						}
+						else{
+							if(prob > -0.5){
+								yRand = 1;
+							}
+							else{
+								yRand = -1;
+							}
+						}
+					}
+					else{
+						prob = rndobj.nextDouble();
+						if(prob < quot){
+							yRand = 1;
+						}
+						else{
+							if(prob < 0.5){
+								yRand = 1;
+							}
+							else{
+								yRand = -1;
+							}
+						}
+					}*/
 					int xRand = rndobj.nextInt(3) - 1;
 					int yRand = rndobj.nextInt(3) - 1;
 					
@@ -115,16 +178,88 @@ public class Life extends PApplet
 		//1. recovery
 		//2. death
 		//3. data graphing
+		deaths = 0;
 		for(int a = 0; a < width/cellDim; a++)
 		{
 			for(int b = 0; b < height/cellDim; b++)
 			{
 				grid[a][b].recovery();
 				grid[a][b].death();
-				grid[a][b].graphing();
+				susceptiblePopulation += grid[a][b].getSusceptiblePopulation();
+				infectedPopulation += grid[a][b].getInfectedPopulation();
+				recoveredPopulation += grid[a][b].getRecoveredPopulation();
+				deaths = grid[a][b].getDeaths();
 			}
 		}
+		graphing(susceptiblePopulation, infectedPopulation, recoveredPopulation, deaths);
+		count++;
 	}
+	
+	public void graphing(int sp, int ip, int rp, int d){
+		dataset(sp, ip, rp, d);
+	}
+	
+	private void dataset(int s, int i, int r, int d) {
+        
+        series1.add(count, s);
+
+        series2.add(count, i);
+
+        series3.add(count, r);
+        
+        series4.add(count, r);
+                   
+    }
+	
+	public static XYDataset createDataset(){
+		final XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series1);
+        dataset.addSeries(series2);
+        dataset.addSeries(series3);
+        dataset.addSeries(series4);
+		return dataset;
+	}
+	
+	private static JFreeChart createChart(final XYDataset dataset) {
+        
+        // create the chart...
+        final JFreeChart chart = ChartFactory.createXYLineChart(
+            "Epidemic Simulation",      // chart title
+            "cycle",                      // x axis label
+            "number of people",                      // y axis label
+            dataset,                  // data
+            PlotOrientation.VERTICAL,
+            true,                     // include legend
+            true,                     // tooltips
+            false                     // urls
+        );
+
+        // NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
+        chart.setBackgroundPaint(Color.white);
+
+        //final StandardLegend legend = (StandardLegend) chart.getLegend();
+        //legend.setDisplaySeriesShapes(true);
+        
+        // get a reference to the plot for further customization
+        final XYPlot plot = chart.getXYPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        //plot.setAxisOffset(new Spacer(Spacer.ABSOLUTE, 5.0, 5.0, 5.0, 5.0));
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        
+        final XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesLinesVisible(0, false);
+        renderer.setSeriesShapesVisible(1, false);
+        plot.setRenderer(renderer);
+
+        // change the auto tick unit selection to integer units only...
+        final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        // OPTIONAL CUSTOMISATION COMPLETED.
+                
+        return chart;
+        
+    }
 
 	static public void main(String[] passedArgs) 
 	{
@@ -138,5 +273,10 @@ public class Life extends PApplet
 		{
 			PApplet.main(appletArgs);
 		}
+		Life obj = new Life();
+		PropGraph pg = new PropGraph("simulation", obj.createChart(obj.createDataset()), chartPanel);
+		pg.pack();
+		RefineryUtilities.centerFrameOnScreen(pg);
+        pg.setVisible(true);
 	}
 }
